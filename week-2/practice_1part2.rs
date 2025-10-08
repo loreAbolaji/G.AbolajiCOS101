f2c8dfa8b44dd9e632a36ff791faa6df59056898
LoreAbolaji <gbolahan.abolaji@pau.edu.ng>
2025-10-08 19:02:08 +0100

    (week2 commit)

--- week-2/practice_1part2.rs
+++ week-2/practice_1part2.rs
@@ -1,6 +1,6 @@
 fn main ()
 {
-	println!("welcome to COS 101")
+	println!("welcome to COS 101");
 
 	println!("the course learning outcome is a as follows:");
 
@@ -9,6 +9,6 @@ fn main ()
                3. understand the different areas of study incomputer,/n
                4. conversant with the applications of computer science ,/n
                5. navigate the career prospects in computer science ,/n
-               6. conversant with computer programming concepts. ":);
+               6. conversant with computer programming concepts. ");
 
 }
