8dd0fbefd61acb86f3734a361ba4f396ffa99602
LoreAbolaji <gbolahan.abolaji@pau.edu.ng>
2025-10-22 21:41:27 +0100

    (rust push)

--- week-4/practice-1/src/main.rs
+++ week-4/practice-1/src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("\n student information management system!") ;
+    //input name 
+    println!("\n please enter your name ");
+    let mut name = String::new();
+    io::stdin()
+    .read_line(&mut name )
+    .expect("failed to read input");
+    println!("your name is :{}",name);
+
+
+    //imnput age
+    println!(" \n enter your age");
+    let mut age = String::new();
+    io::stdin()
+    .read_line(&mut age )
+    .expect("failed to read input");
+    let age :i32 = age 
+    .trim()
+    .parse()
+    .expect("input not an integer");
+    println!("your age is  :{}",age);
+
+
 }
--- week-4/practice-1/target/debug/week-4.d
+++ week-4/practice-1/target/debug/week-4.d
@@ -1,1 +1,1 @@
-C:\Users\HP\Documents\G.AbolajiCOS101\week-4\target\debug\week-4.exe: C:\Users\HP\Documents\G.AbolajiCOS101\week-4\src\main.rs
+C:\Users\HP\Documents\G.AbolajiCOS101\week-4\practice-1\target\debug\week-4.exe: C:\Users\HP\Documents\G.AbolajiCOS101\week-4\practice-1\src\main.rs
