67dd3ad46df3a3dd16652f1d3fdf951c68216a4f
LoreAbolaji <gbolahan.abolaji@pau.edu.ng>
2025-11-26 12:35:22 +0100

    creating and opening a text created using rust command

--- week-9/practice-2/src/main.rs
+++ week-9/practice-2/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::Read;
 fn main(){
+    let mut _file = std::fs::File::create("Welcome_message.txt").expect("create failed");
     let mut file = std::fs::File::open("Welcome_message.txt").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
--- week-9/practice-3/src/main.rs
+++ week-9/practice-3/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
 fn main() {
-    fs::remove_file("data.txt").expect("could not remove file");
+    fs::remove_file("../practice-1/data.txt").expect("could not remove file");
     println!("file is removed");
 }
